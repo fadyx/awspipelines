690e0d48990f07c5f8912cfebe9e21440ffafe0e
fadyx <>
2023-07-10 11:38:08 +0300

    initial commit

---
+++ bin/aws-pipelines.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { AWSpipelinesStack } from '../lib/aws-pipelines-stack';
+
+const app = new cdk.App();
+new AWSpipelinesStack(app, 'AWSpipelinesStack');
