ee99d0ee4fc77d93c8a05f4bc3ae13d5fcd7a40c
fadyx <>
2023-07-10 20:03:32 +0300

    Add build stage with cdk build actions

--- lib/aws-pipelines-stack.ts
+++ lib/aws-pipelines-stack.ts
@@ -1,19 +1,49 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as pipelines from "aws-cdk-lib/aws-codepipeline";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as pipelineActions from "aws-cdk-lib/aws-codepipeline-actions";
 
-export class AWSpipelinesStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AWSpipelinesStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AWSpipelinesQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const pipeline = new pipelines.Pipeline(this, "Pipeline", {
+      pipelineName: "MyPipeline",
+      crossAccountKeys: false,
     });
 
-    const topic = new sns.Topic(this, 'AWSPipelinesTopic');
+    const sourceOutput = new pipelines.Artifact("SourceOutput");
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new pipelineActions.GitHubSourceAction({
+          owner: "fadyx",
+          repo: "awspipelines",
+          output: sourceOutput,
+          branch: "main",
+          actionName: "PipelineSource",
+          oauthToken: cdk.SecretValue.secretsManager("github-token"),
+        }),
+      ],
+    });
+
+    const cdkBuildOutput = new pipelines.Artifact("CdkBuildOutput");
+
+    pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        new pipelineActions.CodeBuildAction({
+          input: sourceOutput,
+          actionName: "CdkBuildAction",
+          outputs: [cdkBuildOutput],
+          project: new codebuild.PipelineProject(this, "CdkBuildProject", {
+            environment: { buildImage: codebuild.LinuxBuildImage.STANDARD_5_0 },
+            buildSpec: codebuild.BuildSpec.fromSourceFilename("build-specs/buildspec.yml")
+          }),
+        }),
+      ],
+    });
   }
 }
